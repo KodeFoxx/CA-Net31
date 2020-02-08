12c9dbab8869ce260654689a7131ff2a7d12d1f2
Yves Schelpe <yves.schelpe@kdg.be>
2020-02-08 20:20:09 +0100

    IServiceCollectionExtensions layer in Application layer

---
+++ Source/Core/Application/Kf.CANetCore31.Core.Application/(dependencyInjection)/IServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Kf.CANetCore31.Core.Application
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplication(
+            this IServiceCollection serviceCollection,
+            IConfiguration configuration = default)
+        {
+            return serviceCollection;
+        }
+    }
+}
