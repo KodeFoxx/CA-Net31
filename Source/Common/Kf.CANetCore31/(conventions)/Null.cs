57bb302adb468d949de21b5b5744a1528dbbedc5
Yves Schelpe <yves.schelpe@kdg.be>
2020-02-08 10:18:39 +0100

    Test IfNullOrEmptyThen and IfNullOrWhiteSpaceThen

---
+++ Source/Common/Kf.CANetCore31/(conventions)/Null.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Kf.CANetCore31
+{
+    public static class Null
+    {
+        public static string NullString
+            => $"(*null)";
+
+        public static KeyValuePair<string, string> NullKeyValuePair
+            => KeyValuePair.Create(NullString, NullString);
+    }
+}
---
+++ Source/Common/Kf.CANetCore31/(extensions)/ObjectExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Kf.CANetCore31.Reflection;
+using LanguageExt;
+using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Kf.CANetCore31
+{
+    public static class ObjectExtensions
+    {
+        /// <summary>
+        /// Returns a <see cref="PropertyInfo"/> object by use of a lambda expression <paramref name="propertySelector"/>.
+        /// </summary>
+        /// <typeparam name="TObject">The type of the object the property comes from.</typeparam>
+        /// <param name="object">The object the property is retrieved from.</param>
+        /// <param name="propertySelector">The lambda expression selecting the property.</param>
+        public static Option<PropertyInfo> GetPropertyInfo<TObject, TResult>(
+            this TObject @object,
+            Expression<Func<TObject, TResult>> propertySelector)
+            => PropertyInfoHelper.GetPropertyInfo(@object, propertySelector);
+
+
+    }
+}
